7f35a45b23796efc535f6ef162ccdb2d3f4e6bbf
JLinnell <linnelljudith@gmail.com>
2025-11-30 10:28:34 -0500

    added logic to display rounds, score and winner

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
+let round = 1
 
 function getComputerChoice (choice) {
     if (choice <= 0.33) {
@@ -14,62 +15,85 @@ function getComputerChoice (choice) {
         console.log("The computer chose rock")
         return "rock"
     } 
-}
+};
 
-
-function getHumanChoice () {
-let userChoice = prompt("Rock, paper,scissors, shoot!").toLowerCase();
-switch(userChoice) {
-  case "paper":
-    console.log("You chose paper");
-    return userChoice
-    break;
-  case "scissors":
-    console.log("You chose scissors");
-    return userChoice
-    break;
-  case "rock":
-    console.log("You chose rock");
-    return userChoice
-    break;
-  default:
-    console.log("I have never heard of that one!");
-    return userChoice
-}
-}
 
 function playGame () {
+    playRock =  document.getElementById('rock').addEventListener('click', function() {
+            const computerSelection = getComputerChoice(Math.random());
+            if (computerSelection === "rock") {
+            $('#output').html(`The computer chose ${computerSelection} too. Wow it's a tie!`);
 
-    if (humanScore >= 5) {
-        console.log("You won the game!!")
-    } else if (computerScore >= 5) {
-        console.log("The computer wins")
+        }   else if (computerSelection === "scissors") {
+            $('#output').html(`The computer chose ${computerSelection}. You win!`);
+            humanScore++
+            document.querySelector('#human-score').textContent = humanScore;
+        }   else {
+            computerScore++
+            $('#output').html( `The computer chose ${computerSelection}. You lose.`);
+            document.querySelector('#computer-score').textContent = computerScore;
+
+        }
+        if (round >= 5) {
+            determineWinner();  
+        } else {
+            round++
+            document.querySelector('#round-number').textContent = round;
+
+        }
+    });
+    playPaper = document.getElementById('paper').addEventListener('click', function() {
+            const computerSelection = getComputerChoice(Math.random());
+            if (computerSelection === "paper") {
+            $('#output').html(`The computer chose ${computerSelection} too. Wow it's a tie!`);
+        }   else if (computerSelection === "rock") {
+            humanScore++
+            $('#output').html(`The computer chose ${computerSelection}. You win!`);
+            document.querySelector('#human-score').textContent = humanScore;
+        }   else {
+            computerScore++
+            $('#output').html( `The computer chose ${computerSelection}. You lose.`);
+            document.querySelector('#computer-score').textContent = computerScore;
+        }
+        if (round >= 5) {
+            determineWinner();  
+        } else {
+            round++
+            document.querySelector('#round-number').textContent = round;
+        }
+    });
+    playScissors = document.getElementById('scissors').addEventListener('click', function() {
+            const computerSelection = getComputerChoice(Math.random());
+            if (computerSelection ===  "scissors") {
+            $('#output').html(`The computer chose ${computerSelection} too. Wow it's a tie!`);
+        }   else if (computerSelection === "paper") {
+            $('#output').html(`The computer chose ${computerSelection}. You win!`);
+            humanScore++
+            document.querySelector('#human-score').textContent = humanScore;
+        }   else {
+            $('#output').html( `The computer chose ${computerSelection}. You lose.`);
+            computerScore++
+            document.querySelector('#computer-score').textContent = computerScore;
+        } 
+        if (round >= 5) {
+            determineWinner();  
+        } else {
+            round++
+            document.querySelector('#round-number').textContent = round;
+        }
+    });
+};
+
+function determineWinner () {
+    if (humanScore > computerScore) {
+        $('#output').html('<h1>You won the whole game!!!</h1>');
+    } else if (humanScore === computerScore) {
+        $('#output').html('<h1>Wow it\'s a tie!</h1>')
     } else {
-        playRound;
+        $('#output').html('<h1>You lost the game. Better luck next time!</h1>')
     }
-}
+};
 
-
-
- document.getElementById('myButton').addEventListener('click', function() {
-    const computerSelection = getComputerChoice(Math.random());
-    const humanSelection = getHumanChoice();
-    playGame();
-    if (humanSelection === computerSelection) {
-    console.log("It's a tie!");
-}   else if (humanSelection === "rock" && computerSelection === "scissors") {
-    console.log("You win!");
-    humanScore++
-}   else if (humanSelection === "scissors" && computerSelection === "paper") {
-    console.log("You win!");
-    humanScore++
-}   else if (humanSelection === "paper" && computerSelection === "rock") {
-    console.log("You win!");
-    humanScore++
-}   else {
-    console.log("You lose")
-    computerScore++
-}
- });
+playGame();
 
 
